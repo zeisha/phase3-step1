58f4ded85f47b38b22f2acec375f26278121a6e0
zeisha <ze.shabani@gmail.com>
2017-07-22 14:32:58 +0330

    34

--- myBlog/settings.py
+++ myBlog/settings.py
@@ -86,19 +86,19 @@ CORS_ORIGIN_ALLOW_ALL = True
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-
 DATABASES = {
-    'default': dj_database_url.config(
-        #default=config('DATABASE_URL')
-        'DATABASE_URL'
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+
+#DATABASES = {
+#    'default': dj_database_url.config(
+#        #default=config('DATABASE_URL')
+#        'DATABASE_URL'
+#    )
+#}
 
 
 # Password validation
