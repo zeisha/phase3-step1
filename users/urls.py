fe03c771eeca0d2dd35a94d70531f8ea317bbdb9
zeisha <ze.shabani@gmail.com>
2017-07-22 14:24:24 +0330

    33

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns= [
     url(r'^register', views.register, name='register'),
     url(r'^login', views.login, name='login'),
-#   url(r'^blog_id$', views.blog_id, name='blog_id'),
+    #url(r'^noone$', views.blog_id, name='blog_id'),
 ]
